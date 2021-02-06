0cb93ea0cdfb52ccd4f99e18f819fbbbdaf1b2bc
slippax <slippa@me.com>
2021-02-06 14:51:40 -0500

    final wave change

--- src/components/common/Waves.js
+++ src/components/common/Waves.js
@@ -4,11 +4,11 @@ const Wave = styled.div`
  display: block;
  position: relative;
  top: ${p=>p.top? '1px' : '-1px'};
- height: 250px;
- background-image: url('data:image/svg+xml;utf8,<svg viewBox="0 0 500 234" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="${p=>p.theme.waveColor}" d="M 0 0 L 466 47 L 466 0 L 0 0 Z" stroke-width="0"></path> <path fill="${p=>p.theme.waveColor}" d="M 465 47 L 684 70 L 684 0 L 465 0 Z" stroke-width="0"></path> </svg>');
+ height: 200px;
+ background-image: url('data:image/svg+xml;utf8,<svg viewBox="0 0 1103 200" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="${p=>p.theme.waveColor}" d="M 0 88 C 173.5 88 173.5 17 347 17 L 347 17 L 347 0 L 0 0 Z" stroke-width="0"></path> <path fill="${p=>p.theme.waveColor}" d="M 346 17 C 557.5 17 557.5 74 769 74 L 769 74 L 769 0 L 346 0 Z" stroke-width="0"></path> <path fill="${p=>p.theme.waveColor}" d="M 768 74 C 935.5 74 935.5 0 1103 0 L 1103 0 L 1103 0 L 768 0 Z" stroke-width="0"></path></svg>');
  background-size: cover;
  background-repeat: no-repeat;
-  transform: ${(p) => (p.flip ? "rotate(180deg)" : "")};
+  transform: ${(p) => (p.flip ? "rotate(180deg) rotateY(180deg)" : "rotateY(180deg)")};
   @media (max-width:499px) {
     background-size: contain;
  }
