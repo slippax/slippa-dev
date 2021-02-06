e70d22f2fcf6e88056cbc967431f9127c6a3e637
slippax <slippa@me.com>
2021-02-06 14:41:40 -0500

    line instead of wave

--- src/components/common/Waves.js
+++ src/components/common/Waves.js
@@ -3,10 +3,9 @@ import styled from "styled-components";
 const Wave = styled.div`
  display: block;
  position: relative;
- top: -0px;
- height: 190px;
- background-image: url('data:image/svg+xml;utf8,<svg viewBox="0 0 500 200" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="${p=>p.theme.waveColor}" d="M 0 0 C 183.5 0 183.5 50 367 50 L 367 50 L 367 0 L 0 0 Z" stroke-width="0"></path> <path fill="${p=>p.theme.waveColor}" d="M 366 50 C 433 50 433 49 500 49 L 500 49 L 500 0 L 366 0 Z" stroke-width="0"></path> </svg>');
- 
+ top: ${p=>p.top? '1px' : '-1px'};
+ height: 250px;
+ background-image: url('data:image/svg+xml;utf8,<svg viewBox="0 0 500 234" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="${p=>p.theme.waveColor}" d="M 0 0 L 466 47 L 466 0 L 0 0 Z" stroke-width="0"></path> <path fill="${p=>p.theme.waveColor}" d="M 465 47 L 684 70 L 684 0 L 465 0 Z" stroke-width="0"></path> </svg>');
  background-size: cover;
  background-repeat: no-repeat;
   transform: ${(p) => (p.flip ? "rotate(180deg)" : "")};
@@ -17,7 +16,7 @@ const Wave = styled.div`
 
 export const WavesEntry = () => {
   return (
-      <Wave flip/>
+      <Wave top flip/>
   );
 };
 
