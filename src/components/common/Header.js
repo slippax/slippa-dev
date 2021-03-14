190adbb1b8f851f624e1c1697a26aef08bd76b69
slippax <slippa@me.com>
2021-03-14 11:01:40 -0400

    zindex fix

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -6,6 +6,7 @@ import Fade from 'react-reveal/Fade';
 const Wrapper = styled.div`
 background:${p=>p.theme.headerColor} !important;
 width:100%;
+z-index:1;
 backdrop-filter: saturate(180%) blur(20px); 
 &:hover {
   background:${p=>p.theme.bodyBackgroundColor} !important;
